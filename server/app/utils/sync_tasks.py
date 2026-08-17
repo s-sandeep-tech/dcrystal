126b00362ba650bbfdda3dd04fb8b9eaa909e8fa
s-sandeep-tech <sandeeps@kaluanjewellers.tech>
2026-08-17 14:18:31 +0530

    fix: update source table reference for old purchase details in sync task query

--- server/app/utils/sync_tasks.py
+++ server/app/utils/sync_tasks.py
@@ -4309,7 +4309,7 @@ def sync_location_wise_old_gold_settlement_transfer_task(task_type_override=None
                 transfer_stwt,
                 transfer_netwt,
                 locationtype
-            FROM Muziris."OldGoldSettlementTransfer"
+            FROM muziris.BI_OldPurchaseDtls;"
         """
 
         start_time = time.time()
